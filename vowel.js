70bf4778980435bee569ef4ee1aabf3c70a81722
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-28 21:06:05 +0600

    2nd commit

--- vowel.js
+++ vowel.js
@@ -1,4 +1,4 @@
-let str = "Education is beautiful"
-let cases = str.toLowerCase
-let result = cases.includes('a') && cases.includes('e') && cases.includes('i') && cases.includes('o') && cases.includes('u')
+let str = 'Education is beautiful';
+let cases = str.toLowerCase();
+let result = cases.includes('a') && cases.includes('e') && cases.includes('i') && cases.includes('o') && cases.includes('u');
 console.log(result)
