5ec3b9d76994486ad885febebe0078d2b9426014
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-28 20:54:20 +0600

    string-task

---
+++ vowel.js
@@ -0,0 +1,4 @@
+let str = "Education is beautiful"
+let cases = str.toLowerCase
+let result = cases.includes('a') && cases.includes('e') && cases.includes('i') && cases.includes('o') && cases.includes('u')
+console.log(result)
