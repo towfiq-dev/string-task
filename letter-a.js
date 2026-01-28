64a9e8dd4dfcad5e13a3da55c8633ea32a41c2a9
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-28 20:36:01 +0600

    string-task

---
+++ letter-a.js
@@ -0,0 +1,6 @@
+let sentence = 'Count how many times a string has the letter a'
+//let parts = sentence.split('a')
+//console.log(parts)
+//let count = parts.length-1
+console.log(sentence.length)
+//console.log(count)
