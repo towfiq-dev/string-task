37fa8fd102c8bc9434d4291dd9552685951b5d70
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-29 09:40:55 +0600

    added number type

---
+++ x-ray.js
@@ -0,0 +1,17 @@
+// let text = 'X-ray is expensive'
+// let newtext = text.replaceAll('x', 'y').replaceAll('X', 'Y')
+// console.log(newtext)
+// const friends =['rohim', 'korim', 'sayem','rahim']
+
+// for(const friend of friends){
+
+//   console.log(friend)
+  let numss = '100'
+  let resultsss = Number(numss)
+  console.log(resultsss)
+  let num ='999.9998'
+  let nums =Number(num)
+  let dosomik =parseFloat(nums)
+  let result =parseInt(dosomik)
+  //let dosomik =parseFloat(result)
+  console.log(result)
